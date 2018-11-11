88f35cdac11a77683c11239b4b52522ced756839
Daniel Bogomazov <danielbogoamzov@gmail.com>
2018-11-11 02:33:09 -0500

    Setup script for fresh pull

---
+++ create_db.sql
@@ -0,0 +1,58 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE genre (
+	genre_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	subgenre_name TEXT NOT NULL
+);
+
+CREATE TABLE artist (
+	artist_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE producer (
+	producer_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE label (
+	label_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE recording (
+	recording_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
+	name TEXT,	
+	release_date DATE
+);
+
+CREATE TABLE b_artist_genre (
+	artist_id uuid REFERENCES artist (artist_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	genre_id uuid REFERENCES genre (genre_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT b_artist_genre_pkey PRIMARY KEY (artist_id, genre_id)
+);
+
+CREATE TABLE b_recording_label (
+	recording_id uuid REFERENCES recording (recording_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	label_id uuid REFERENCES label (label_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT b_recording_label_pkey PRIMARY KEY (recording_id, label_id)
+);
+
+CREATE TABLE b_recording_producer (
+	recording_id uuid REFERENCES recording (recording_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	producer_id uuid REFERENCES producer (producer_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT b_recording_producer_pkey PRIMARY KEY (recording_id, producer_id)
+);
+
+CREATE TABLE b_recording_genre (
+	recording_id uuid REFERENCES recording (recording_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	genre_id uuid REFERENCES genre (genre_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT b_recording_genre_pkey PRIMARY KEY (recording_id, genre_id)
+);
+
+CREATE TABLE b_recording_artist (
+	recording_id uuid REFERENCES recording (recording_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	artist_id uuid REFERENCES artist (artist_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT b_recording_artist_pkey PRIMARY KEY (recording_id, artist_id)
+);
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## Set up the apollo database using postgres
+
+# TODO MAKE SURE TO REMOVE THIS BEFORE PUSHING AAAAAAA
+dropdb "apollo"
+dropuser "apollo"
+
+# check if postgres is installed
+which -s psql
+if [ ! $? -eq 0 ]
+then
+	echo "ERROR - postgres not installed"
+	exit 1
+fi
+
+# check if role exists
+# if not - create it
+psql -qt -c "SELECT * FROM pg_roles WHERE rolname='apollo'" | cut -d \| -f 1 | grep -qw apollo
+if [ ! $? -eq 0 ]
+then
+	createuser "apollo" -sd
+fi
+
+# check if apollo db exists
+# if not - create it
+psql -lqt | cut -d \| -f 1 | grep -qw apollo
+if [ ! $? -eq 0 ]
+then
+	createdb -O "apollo" -U "apollo" -E "UTF8" "apollo"
+fi
+
+# check if tables exist
+# if not - create them
+psql -q -U "apollo" -d "apollo" -c "SELECT * FROM pg_tables WHERE tablename = 'artist' AND tableowner = 'apollo';" |  grep -qw "(0 rows)"
+if [ $? -eq 0 ]
+then
+	psql -qt -U "apollo" -d "apollo" < create_db.sql
+fi

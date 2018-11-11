11e2fb50a78013f172984fb551185c71a61dce56
Daniel Bogomazov <danielbogoamzov@gmail.com>
2018-11-11 18:40:18 -0500

    Removed subgenre

--- create_db.sql
+++ create_db.sql
@@ -2,8 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE genre (
 	genre_id uuid DEFAULT uuid_generate_v1() PRIMARY KEY,
-	name VARCHAR(20) NOT NULL,
-	subgenre_name TEXT NOT NULL
+	name TEXT NOT NULL,
 );
 
 CREATE TABLE artist (
